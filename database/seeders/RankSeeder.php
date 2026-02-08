fb353fae5545cc89a1f8731356984d3d4351dbe8
Camilo Ospina <camospinac@outlook.com>
2026-02-08 12:19:45 -0500

    48598459daasd

--- database/seeders/RankSeeder.php
+++ database/seeders/RankSeeder.php
@@ -12,14 +12,21 @@ class RankSeeder extends Seeder
         Rank::create([
             'name' => 'Bronce',
             'required_referrals' => 10,
-            'reward_description' => 'Bono del 10% sobre la inversión de tus referidos',
-            'reward_percentage' => 10.00,
+            'reward_description' => 'Bono de 400.000 COP',
+            'reward_amount' => 400000.00,
         ]);
         Rank::create([
             'name' => 'Plata',
-            'required_referrals' => 25,
-            'reward_description' => 'Bono del 10% sobre la inversión de tus referidos',
-            'reward_percentage' => 10.00,
+            'required_referrals' => 20,
+            'reward_description' => 'Bono de 600.000 COP',
+            'reward_amount' => 600000.00,
+        ]);
+
+        Rank::create([
+            'name' => 'Oro',
+            'required_referrals' => 30,
+            'reward_description' => 'Bono de 1.000.000 COP',
+            'reward_amount' => 1000000.00,
         ]);
     }
 }
